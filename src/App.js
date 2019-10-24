649059a00731115c21a7d70b925c8b0053e2c7cb
aaronmadsen1 <aaronmadsen2@gmail.com>
2019-10-24 16:52:16 -0600

    add teams functionality, aaron 7

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React, { useEffect } from "react";
-import ReactDOM from "react-dom";
-import axios from "axios";
+import React, { useEffect } from "react"
+import ReactDOM from "react-dom"
+import axios from "axios"
 
-import { DragDropContext, Droppable } from "react-beautiful-dnd";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { DragDropContext, Droppable } from "react-beautiful-dnd"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-import TitleBar from "./components/TitleBar";
-import StudentDraggable from "./components/StudentDraggable";
-import TeamList from "./components/TeamList";
+import TitleBar from "./components/TitleBar"
+import StudentDraggable from "./components/StudentDraggable"
+import TeamList from "./components/TeamList"
 
-import mockData from "./mockData";
-import Icons from "./helpers/icons";
-import { existsTypeAnnotation } from "@babel/types";
+import mockData from "./mockData"
+import Icons from "./helpers/icons"
+import { existsTypeAnnotation } from "@babel/types"
 
 const App = () => {
-  Icons();
+  Icons()
 
-  const [student, setStudent] = React.useState("");
-  const [students, setStudents] = React.useState([]);
+  const [student, setStudent] = React.useState("")
+  const [students, setStudents] = React.useState([])
+  const [teamsEntry, setTeamsEntry] = React.useState("")
+  const [teamsArray, setTeamsArray] = React.useState([])
 
   const renderStudents = () => {
-    const noTeam = students.filter(student => student.team === 0);
+    const noTeam = students.filter(student => student.team === 0)
     return noTeam.map((student, index) => {
       return (
         <StudentDraggable
@@ -30,9 +32,9 @@ const App = () => {
           delete={deleteStudent}
           // id={student.id}
         />
-      );
-    });
-  };
+      )
+    })
+  }
 
   //   fetch("https://mar-todo-api.herokuapp.com/todos")
   //     .then(response => response.json())
@@ -48,49 +50,49 @@ const App = () => {
   // });
 
   const handleSubmit = e => {
-    e.preventDefault();
+    e.preventDefault()
     if (student === "") {
-      return console.log("UNSUCCESSFUL SUBMIT:\nNO NAME\n");
+      return console.log("UNSUCCESSFUL SUBMIT:\nNO NAME\n")
     }
     axios
-      .post("http://127.0.0.1:5000/student", {
+      .post("https://dragn-drop-teams.herokuapp.com/student", {
         name: student,
         team: 0
       })
       .then(response => {
-        console.log("successful post", response);
-        setStudents([...students, response.data]);
-        setStudent("");
+        console.log("successful post", response)
+        setStudents([...students, response.data])
+        setStudent("")
       })
       .catch(error => {
-        console.log("student post error", error);
-      });
-  };
+        console.log("student post error", error)
+      })
+  }
 
   const getStudents = () => {
     axios
-      .get("http://127.0.0.1:5000/students")
+      .get("https://dragn-drop-teams.herokuapp.com/students")
       .then(response => {
-        console.log("setStudent response", response.data);
-        setStudents(response.data);
+        console.log("setStudent response", response.data)
+        setStudents(response.data)
       })
       .catch(error => {
-        console.log("setStudent error", error);
-      });
-  };
+        console.log("setStudent error", error)
+      })
+  }
 
   useEffect(() => {
-    getStudents();
-  }, []);
+    getStudents()
+  }, [])
 
   // FUNCTION TO PREVENT AUTO-SORTING LISTS WHEN STUDENT IS DROPPED IN A TEAM
   const reorder = (list, startIndex, endIndex) => {
-    const result = Array.from(list);
-    const [removed] = result.splice(startIndex, 1);
-    result.splice(endIndex, 0, removed);
+    const result = Array.from(list)
+    const [removed] = result.splice(startIndex, 1)
+    result.splice(endIndex, 0, removed)
 
-    return result;
-  };
+    return result
+  }
 
   // const handleSubmit = e => {
   //   e.preventDefault();
@@ -123,27 +125,40 @@ const App = () => {
   //   setStudents: students.filter(student => student.id !==id)
   // )
   // }
-  const deleteStudent = id =>
+
+  const deleteStudent = id => {
     axios
-      .delete(`http://127.0.0.1:5000/student/${id}`)
+      .delete(`https://dragn-drop-teams.herokuapp.com/student/${id}`)
       .then(response => {
-        console.log("student deleted", response);
-        setStudents(students.filter(student => student.id !== id));
+        console.log("student deleted", response)
+        setStudents(students.filter(student => student.id !== id))
       })
       .catch(error => {
-        console.log("error deleting student", error);
-      });
+        console.log("error deleting student", error)
+      })
+  }
+
+  const setNumberOfTeams = e => {
+    e.preventDefault()
+    let numberOfTeamsArray = []
+    for (let i = teamsEntry; i > 0; i--) {
+      numberOfTeamsArray.unshift(i)
+    }
+    setTeamsArray(numberOfTeamsArray)
+    renderTeams()
+  }
 
   const renderTeams = () => {
-    const teamNums = [1, 2, 3];
-    return teamNums.map((team, idx) => {
-      return <TeamList key={idx} students={students} number={team} />;
-    });
-  };
+    return teamsArray.map((team, idx) => {
+      //   // const teamNums = [1, 2]
+      // return teamNums.map((team, idx) => {
+      return <TeamList key={idx} students={students} number={team} />
+    })
+  }
 
   const onDragEnd = result => {
     if (!result.destination) {
-      return;
+      return
     }
 
     // CALLING REORDER FUNCTION -- NOT WORKING
@@ -155,9 +170,9 @@ const App = () => {
 
     const droppedStudent = students.find(
       student => student.id === result.draggableId
-    );
-    droppedStudent.team = +result.destination.droppableId;
-  };
+    )
+    droppedStudent.team = +result.destination.droppableId
+  }
 
   // * * * * * *  Activates top title bar  * * * * * *
   React.useEffect(() => {
@@ -165,27 +180,26 @@ const App = () => {
   })
 
   document.addEventListener("scroll", () => {
-    document.documentElement.dataset.scroll = window.scrollY;
-  });
+    document.documentElement.dataset.scroll = window.scrollY
+  })
 
   // * * * * * *  Scroll to top on refresh  * * * * * *
   window.onbeforeunload = function() {
     window.scrollTo(0, 0)
   }
-
   return (
     <div className="page-wrapper">
       <TitleBar />
       <DragDropContext onDragEnd={onDragEnd}>
         <div className="add-number-of-teams-wrapper">
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={setNumberOfTeams}>
             <div className="add-teams-form-wrapper">
               <div className="teams-input">
                 <input
                   type="text"
                   placeholder="Number of Teams"
-                  // value={student}
-                  // onChange={e => setStudent(e.target.value)}
+                  value={teamsEntry}
+                  onChange={e => setTeamsEntry(e.target.value)}
                 />
               </div>
 
@@ -245,14 +259,26 @@ const App = () => {
             {/* <TeamList students={students} number={"1"} />
             <TeamList students={students} number={"2"} />
             <TeamList students={students} number={"3"} />
-            {/* <TeamList students={students} number={"4"} />
+            <TeamList students={students} number={"4"} />
             <TeamList students={students} number={"5"} />
             <TeamList students={students} number={"6"} /> */}
           </div>
         </div>
       </DragDropContext>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
+
+// Database link: "https://dragn-drop-teams.herokuapp.com"
+
+// GET: https://dragn-drop-teams.herokuapp.com/students
+// GET: https://dragn-drop-teams.herokuapp.com/student/${id}
+// POST: https://dragn-drop-teams.herokuapp.com/student
+// PUT: https://dragn-drop-teams.herokuapp.com/student/${id}
+// DELETE: https://dragn-drop-teams.herokuapp.com/student/${id}
+
+// "id" : int
+// "name" : string
+// "team" : int
